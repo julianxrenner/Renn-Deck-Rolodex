a847312d59e79dbfcdaa96cf08fbc12aeb67d261
julianxrenner <julesman18@gmail.com>
2026-03-11 20:46:49 -0400

    changing back to touchable opacity

--- RennDeck/components/ContactList.jsx
+++ RennDeck/components/ContactList.jsx
@@ -3,25 +3,23 @@ import {
   View,
   TextInput,
   Text,
+  Button,
   TouchableOpacity,
   ScrollView,
   FlatList,
 } from "react-native";
 
-import { Link } from "@react-navigation/native";
-import { Button } from "@react-navigation/elements";
 
 const ContactList = (props) => {
   const contactButtons = props.contacts.map((person) => (
-    <Button
-      screen=""
+    <TouchableOpacity
       key={person.id}
       className="bg-blue-500 rounded-lg p-4 shadow-md m-1 mx-4"
     >
       <Text className="text-center text-white text-lg font-bold">
         {person.name}
       </Text>
-    </Button>
+    </TouchableOpacity>
   ));
   return (
     <ScrollView className="my-5 bg-gray-200 rounded-lg">
